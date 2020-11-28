f7440dc72313af2c991796bb61c43e6f623e6cee
TOLIVEIN <TOLIVEIN@outlook.com>
2020-11-28 19:33:25 +0800

    feat(*) add tinyMCE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
+import { InspirationComponent } from './components/inspiration/inspiration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'read',
+    component: OverviewComponent,
+    // outlet: 'main',
+  },
+  {
+    path: 'write',
+    component: InspirationComponent,
+    // outlet: 'main',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { EditorModule } from '@tinymce/tinymce-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { MainComponent } from './layout/main/main.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { DetailComponent } from './components/detail/detail.component';
 import { CardComponent } from './components/card/card.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { InspirationComponent } from './components/inspiration/inspiration.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +20,10 @@ import { CardComponent } from './components/card/card.component';
     OverviewComponent,
     DetailComponent,
     CardComponent,
+    InspirationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, EditorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
