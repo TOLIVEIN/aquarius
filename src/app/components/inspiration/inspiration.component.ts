6f53708c51a63300684b36911e99e5c4ef278c82
TOLIVEIN <TOLIVEIN@outlook.com>
2020-11-29 11:28:55 +0800

    feat(*) add wangEditor

--- src/app/components/inspiration/inspiration.component.ts
+++ src/app/components/inspiration/inspiration.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import Editor from 'wangeditor';
 
 @Component({
   selector: 'app-inspiration',
   templateUrl: './inspiration.component.html',
-  styleUrls: ['./inspiration.component.less']
+  styleUrls: ['./inspiration.component.less'],
 })
 export class InspirationComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const editor = new Editor('#editorMenu', '#editor');
+    // 或者 const editor = new E( document.getElementById('div1') )
+    editor.create();
   }
-
 }
