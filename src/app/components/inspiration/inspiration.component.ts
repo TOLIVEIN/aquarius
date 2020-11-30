a1c495b5dd0baab2bdb7196cfd0ea197957335f5
TOLIVEIN <TOLIVEIN@outlook.com>
2020-11-30 18:53:55 +0800

    feat(*) add get html

--- src/app/components/inspiration/inspiration.component.ts
+++ src/app/components/inspiration/inspiration.component.ts
@@ -7,19 +7,26 @@ import * as hljs from 'highlight.js';
   styleUrls: ['./inspiration.component.less'],
 })
 export class InspirationComponent implements OnInit {
+
+  private editor!: Editor;
   constructor() {}
 
   ngOnInit(): void {
-    const editor = new Editor('#editorMenu', '#editor');
+    this.editor = new Editor('#editorMenu', '#editor');
     // const editor = new Eeditor(
     //   document.getElementById('editorMenu'),
     //   document.getElementById('editor')
     // );
 
     // editor.config.height = 1000;
-    editor.highlight = hljs;
+    this.editor.highlight = hljs;
 
-    editor.config.placeholder = '输入文章内容';
-    editor.create();
+    this.editor.config.placeholder = '输入文章内容';
+    this.editor.create();
+  }
+  public getHtml(): void {
+    const html = this.editor.txt.html();
+    console.log(html);
+    // console.log(e);
   }
 }
