408993c65712dfebe97c187d00da78f8a67fd7bf
TOLIVEIN <TOLIVEIN@outlook.com>
2020-12-25 17:07:39 +0800

    refactor(*) remove some comments

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -14,7 +14,6 @@ export class SignInComponent implements OnInit {
 
   tags!: TagData;
   articles!: ArticleData;
-  // SignInForm: FormGroup;
   constructor(
     private dataService: DataService,
     private authService: AuthService
@@ -22,38 +21,21 @@ export class SignInComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onSubmit(formData: FormGroup): void {
-    // console.log('sign in', this.authService.signInForm);
+  onSubmit(): void {
     this.signIn();
     this.saveCookie();
     this.authService.signInForm.reset();
-    // console.log(formData)
   }
 
   signIn(): void {
     this.authService.getAuth().subscribe((res) => {
-      // console.log(data);
       this.token = res.data.token;
       localStorage.setItem('token', this.token);
       console.log(localStorage);
-
-      // this.dataService.getUsers().subscribe(r => {
-      //   console.log(r);
-      // });
     });
-
-    // this.dataService.getTags().subscribe((tags) => {
-    //   this.tags = tags.data;
-    //   console.log(this.tags);
-    // });
-    // this.dataService.getArticles().subscribe((articles) => {
-    //   this.articles = articles.data;
-    //   console.log(this.articles);
-    // });
   }
 
   saveCookie(): void {
-    // console.log(data)
     const expires = new Date();
     expires.setTime(expires.getTime() + 10 * 3600000 * 24);
     // console.log(`username=${this.authService.signInForm.value.username}; password=${this.authService.signInForm.value.password}; expires=${expires}`)
