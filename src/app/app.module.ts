10087cfb8082e0c9dc0965770da8ea631dd10250
TOLIVEIN <TOLIVEIN@outlook.com>
2020-12-31 16:04:43 +0800

    feat(*) add card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,72 @@
+// import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtModule } from '@auth0/angular-jwt';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
+import { CategoryComponent } from './components/category/category.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { InspirationComponent } from './components/inspiration/inspiration.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
-import { CategoryComponent } from './components/category/category.component';
-// import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
-import { HttpClientModule } from '@angular/common/http';
-import { JwtModule } from '@auth0/angular-jwt';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { BaseGuard } from './guards/base.guard';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatChipsModule } from '@angular/material/chips';
-import {MatIconModule} from '@angular/material/icon';
 
 export function tokenGetter(): string | null {
-  return localStorage.getItem('access_token');
+    return localStorage.getItem('access_token');
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
-    OverviewComponent,
-    DetailComponent,
-    CardComponent,
-    InspirationComponent,
-    CategoryComponent,
-    SignInComponent,
-    SignUpComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    EditorModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatChipsModule,
-    MatIconModule,
-    //  HighlightModule
-    JwtModule.forRoot({
-      config: {
-        tokenGetter,
-        allowedDomains: ['localhost:4000'],
-        disallowedRoutes: ['localhost:4000/api/auth'],
-      },
-    }),
-  ],
-  providers: [
-    // BaseGuard
-    // {
-    //   provide: HIGHLIGHT_OPTIONS,
-    //   useValue: {
-    //     fullLibraryLoader: () => import('highlight.js'),
-    //   },
-    // },
-  ],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        MainComponent,
+        OverviewComponent,
+        DetailComponent,
+        InspirationComponent,
+        CategoryComponent,
+        SignInComponent,
+        SignUpComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        EditorModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatChipsModule,
+        MatIconModule,
+        MatCardModule,
+        //  HighlightModule
+        JwtModule.forRoot({
+            config: {
+                tokenGetter,
+                allowedDomains: ['localhost:4000'],
+                disallowedRoutes: ['localhost:4000/api/auth'],
+            },
+        }),
+    ],
+    providers: [
+        // BaseGuard
+        // {
+        //   provide: HIGHLIGHT_OPTIONS,
+        //   useValue: {
+        //     fullLibraryLoader: () => import('highlight.js'),
+        //   },
+        // },
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
