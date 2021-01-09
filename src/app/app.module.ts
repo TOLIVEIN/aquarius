36baf1390666def4f3f4c3bc756d5d6319e14fdf
TOLIVEIN <TOLIVEIN@outlook.com>
2021-01-09 08:29:02 +0800

    feat(*) header style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -51,11 +52,13 @@ export function tokenGetter(): string | null {
         FormsModule,
         ReactiveFormsModule,
         BrowserAnimationsModule,
+        MatButtonModule,
         MatChipsModule,
         MatIconModule,
         MatCardModule,
         MatFormFieldModule,
         MatAutocompleteModule,
+
         //  HighlightModule
         JwtModule.forRoot({
             config: {
