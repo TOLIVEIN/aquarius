c9cb3416e2c36d6c7c07c465bd65a9bbab639911
TOLIVEIN <TOLIVEIN@outlook.com>
2021-01-16 08:19:09 +0800

    chore(*) env

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ConfigService {
     configUrl = 'assets/config.json';
-    requestUrl = 'http://localhost:8000';
+    requestUrl = environment.baseUrl;
+    // requestUrl = 'http://localhost:8000';
     // requestUrl = 'http://42.192.102.206:8080';
 
     constructor(private http: HttpClient) {}
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     baseUrl: 'http://42.192.102.206:8080',
 };
