f72004eeb9ef9b951fac50914340eacfc6ee077e
TOLIVEIN <TOLIVEIN@outlook.com>
2021-03-06 15:04:13 +0800

    feat(*) add themes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -13,6 +13,29 @@ export class HeaderComponent implements OnInit {
 
     isSignIn: boolean;
 
+    themes = [
+        'turquoise',
+        'emerald',
+        'peter-river',
+        'amethyst',
+        'wet-asphalt',
+        'green-sea',
+        'nephritis',
+        'belize-hole',
+        'wisteria',
+        'midnight-blue',
+        'sun-flower',
+        'carrot',
+        'alizarin',
+        'clouds',
+        'concrete',
+        'orange',
+        'pumpkin',
+        'pomeganate',
+        'silver',
+        'asberstos',
+    ];
+
     constructor(
         private dataService: DataService,
         private authService: AuthService,
@@ -27,5 +50,15 @@ export class HeaderComponent implements OnInit {
     signOut(): void {
         this.authService.signOut();
         this.router.navigate(['overview']);
+    }
+    changeTheme(themeColor: string) {
+        document.body.style.setProperty(
+            '--theme-color',
+            `var(--color-${themeColor})`
+        );
+        document.body.style.setProperty(
+            '--theme-rgb',
+            `var(--rgb-${themeColor})`
+        );
     }
 }
